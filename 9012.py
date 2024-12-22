442165b1c0480c69b967487df877f5ac1f3f1c17
LEE JI HUN <lgh99669581@gmail.com>
2024-12-23 04:23:12 +0900

    9012

---
+++ 9012.py
@@ -0,0 +1,27 @@
+import sys
+input = input = sys.stdin.readline
+
+def check_vps(ps):
+    stack = []
+    for char in ps:
+        if char == '(':
+            stack.append(char)
+        elif char == ')':
+            if not stack :
+                return "NO"
+            else:
+                stack.pop()
+    if not stack :
+        return "YES"
+    else:
+        return "NO"
+                
+
+n=int(input())
+results = []
+for i in range(n):
+    arr = list(input())
+    results.append(check_vps(arr))
+    print(results[i])
+    
+    
